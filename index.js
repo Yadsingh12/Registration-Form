d89d17321de011aa6b1c853a66b048364d97f7cc
Yadsingh12 <your_donotreply_email_id>
2024-08-31 14:10:20 +0530

    feat: add route to serve index.html file from /pages directory

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ dotenv.config();
 
 const port = process.env.PORT || 3000;
 
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + "/pages/index.html");
+})
+
 app.listen(port, ()=>{
     console.log(`server is running on port ${port}`);
 })
